2f95443f4b7a35fa8f9ce0dc60a4d8b592c7b272
Vitto95 <grimaldi.vitto@gmail.com>
2021-05-27 21:26:49 +0200

    aggiunto fontawesome

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -2,10 +2,19 @@
   <!-- dati per film -->
   <div class="film-data" v-if="typeDataCard === 'movie'">
     <p>Tipologia dato: {{ typeDataCard }}</p>
+    <img :src="getImageUrl(cardObject.poster_path)" alt="img" />
     <p>Titolo: {{ cardObject.title }}</p>
     <p>Titolo originale: {{ cardObject.original_title }}</p>
     <p>Lingua Originale: {{ cardObject.original_language }}</p>
     <p>Voto medio: {{ cardObject.vote_average }}</p>
+    <div>
+      <p>output stella piena</p>
+      <i class="fas fa-star"></i>
+    </div>
+    <div>
+      <p>output stella vuota</p>
+      <i class="far fa-star"></i>
+    </div>
     <p>******************************************************</p>
   </div>
 </template>
@@ -16,6 +25,18 @@ export default {
   props: {
     cardObject: Object,
     typeDataCard: String,
+  },
+  data() {
+    return {
+      baseUrl: "https://image.tmdb.org/t/p/",
+      baseWidth: "w300",
+    };
+  },
+  methods: {
+    getImageUrl(dinamicUrl) {
+      let completeUrl = this.baseUrl + this.baseWidth + dinamicUrl;
+      return completeUrl;
+    },
   },
 };
 </script>
